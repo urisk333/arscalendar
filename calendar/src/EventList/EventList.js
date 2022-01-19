33648b5dd94c0c5031bc61bf01e565df4a82e9aa
urisk <carst333@gmail.com>
2022-01-19 12:03:47 +0100

    [FRONTEND: EVENTLIST]: Filter and group the events on a day basis

--- calendar/src/EventList/EventList.js
+++ calendar/src/EventList/EventList.js
@@ -1,9 +1,35 @@
 import React from 'react';
+import { useState, useEffect } from 'react';
 
 function EventList ({ events }) {
 
+  const [newEvents, setNewEvents] = useState(events);
+  
+  useEffect(() => {
+    setNewEvents(events);
+  }, [events]);
+  
+  let groupedEvents;
+  if (newEvents) {
+
+    const filteredEvents = newEvents.filter(event =>  
+      (new Date(event.start.dateTime).getTime() >= Date.now()) && (new Date(event.end.dateTime).getTime() <= new Date().setDate(new Date().getDate() + 7)) 
+      ?
+      event : null)
+
+    groupedEvents = filteredEvents.reduce((groups, event) => {
+      const date = new Date(event.start.dateTime).getDay();
+
+      if (!groups[date]) {
+        groups[date] = [];
+      }
+      groups[date].push(event);
+      return groups;
+    }, {}); 
+  }
+
   return (
-    <div>
+    <div className="eventlist-container">
       
     </div>
   );
