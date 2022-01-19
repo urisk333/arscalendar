36a1bb67db75ce57c91f3183eae77cb151159a06
urisk <carst333@gmail.com>
2022-01-19 11:34:45 +0100

    [FRONTEND: COMPONENTS]: Set up all necessary components - EventList and EventItem

---
+++ calendar/src/EventItem/EventItem.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function EventItem () {
+
+  return (
+    <div>
+      
+    </div>
+  );
+}
+
+export default EventItem;
---
+++ calendar/src/EventList/EventList.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function EventList () {
+
+  return (
+    <div>
+      
+    </div>
+  );
+}
+
+export default EventList;
